6dc3095ecff18fc432af68426cb1e656ed90904d
Beraldino <150296949+Beraldino@users.noreply.github.com>
2023-12-11 19:19:28 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+array1=['pen','paper','book','bottle'];
+randomNo = Math.floor((Math.random()*array1.length)+1);
+
+Element_of_array = array1[randomNo];
+
+console.log(randomNumber);
+
+document.getElementById('showsketch'). innerHTML = 'Esboço a ser desenhado';
+
+timerConter = 0;
+TimerCheck = '';
+drawSketch = '';
+score = 0;
